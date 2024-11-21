1058bd5cef5c30e0021ecb846cb82c421741568d
Corsicaa <jonathanbernardini92@gmail.com>
2024-11-21 15:59:12 +0100

    corsicaa/initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ZakHarmony - BackOffice</title>
+        <meta
+          name="description"
+          content="ZakHarmony - Votre plateforme musicale"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <h1>ZakHarmony</h1>
+    </>
+  );
+}
